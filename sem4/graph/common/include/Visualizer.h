e4957ac28d9ed90bc03073a1c3ee235e0d22fd98
ivanchetvergov <ivar5165885@gmail.com>
2026-01-22 23:43:34 +0300

    feat: new visualizer

--- sem4/graph/common/include/Visualizer.h
+++ sem4/graph/common/include/Visualizer.h
@@ -24,36 +24,20 @@ struct DrawData {
 
 };
 
-class Visualizer {
-    using DistanceMatrix = std::vector<std::vector<std::optional<double>>>;
-
-public:
-    static void drawGraph(Graph const& graph, DrawData const& data);
-    static void drawGraphWithPath(Graph const& graph, DrawData const& data);
-    static void drawGraphWithPaths(Graph const& graph, DrawData const& data);
-    static void drawColoredGraph(Graph const& graph, DrawData const& data);
-
-    static void drawAdjacencyMatrix(Graph const& graph, DrawData const& data);
-    static void drawWeightMatrix(Graph const& graph, DrawData const& data);
-    static void drawShimbellMatrix(DistanceMatrix const& shimMatrix, DrawData const& data);
-
-
-    static void drawFlowNetwork(FlowNetwork const& network, DrawData const& data);
-    static void drawFlowNetworkWithPath(FlowNetwork const& network, DrawData const& data);
-    static void drawCapacityMatrix(FlowNetwork const& network, DrawData const& data);
-    static void drawCostMatrix(FlowNetwork const& network, DrawData const& data);
-
-private:
-    static void exportColors(DrawData const& data, std::vector<int> const& vertices);
-    static void exportEdges(Graph const& graph, DrawData const& data);
-    static void exportPath(DrawData const& data);
-    static void exportPaths(DrawData const& data);
-    static void exportAddedEdges(DrawData const& data);
-    static void exportMatrix(std::vector<std::vector<double>> const& matrix, DrawData const& data);
-    static void exportFlow(FlowNetwork const& network, DrawData const& data);
-
-    static void drawMatrixHelper(std::vector<std::vector<double>> const& matrix,
-                                 DrawData const& data,
-                                 std::string const& defaultTitle);
+enum class VisualizationType {
+    Graph,
+    FlowNetwork,
+    Animation
 };
+
+class Visualizer {
+public:
+    static void visualize(DrawData const& data, bool directed, VisualizationType type);
+    static void drawPaths(DrawData const& data, bool directed, VisualizationType type);
+    static void drawColoredGraph(DrawData const& data, bool directed);
+
+    static void drawMatrix(DrawData const& data, std::string const& defaultTitle);
+
+};
+
 }  // namespace graph
